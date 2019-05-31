331c5ff81ad0eaf7f861eeb2aaf6cedbfda84d1e
rohto <rohto111117@gmail.com>
2019-05-31 17:28:45 +0900

    20190531 project first commited

---
+++ core/scraping/media/ITMediaGetter.py
@@ -0,0 +1,17 @@
+'''
+Created on 2019/05/31
+
+@author: Rohto
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
